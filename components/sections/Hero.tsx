9cde2dc7edade3a5f90cc5b5cfd364cafc9ae540
Okpeyemi <maqsoud.tawaliou@epitech.eu>
2025-03-13 19:46:31 +0100

    feat: update the products style

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -5,7 +5,7 @@ import WhatsAppGreenIcon from "@/public/icons/whatsappIconGreen.svg";
 
 const Hero = () => {
   return (
-    <div className="bg-gradient-to-b from-primary to-foreground max-xl:mt-20 mt-24 h-[700px] max-md:h-[500px] flex flex-col items-start justify-center text-background responsive space-y-10">
+    <div className="bg-gradient-to-b from-primary to-foreground max-lg:mt-16 max-xl:mt-20 mt-24 h-[700px] max-md:h-[500px] flex flex-col items-start justify-center text-background responsive space-y-10">
       <div className="text">
         <h1>Médical & Santé.</h1>
         <h1>Beauté, Cosmétiques & Soins personnels.</h1>
